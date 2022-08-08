de596d18108a666064eb85e6c706982caa91a99d
Dusan95z <dusan204z@gmail.com>
2022-08-08 21:32:43 +0200

    responsive navigation done

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ document.addEventListener('scroll', (e) => {
 headerArrow.addEventListener('click', (e) => {
   window.scrollTo(0, 0);
 });
+
+// RESPONSIVE NAVIGATION BURGER MENU
+
+const header = document.querySelector('.header');
+const nav = document.querySelector('.header-nav');
+const burgerMenu = document.querySelector('.header-burgerMenu');
+const closeBurgerMenu = document.querySelector('.header-closeBurgerMenu');
+
+burgerMenu.addEventListener('click', (e) => {
+  header.classList.add('header-active');
+  nav.classList.add('nav-active');
+  burgerMenu.style.display = 'none';
+  closeBurgerMenu.style.display = 'flex';
+});
+
+closeBurgerMenu.addEventListener('click', (e) => {
+  header.classList.remove('header-active');
+  nav.classList.remove('nav-active');
+  burgerMenu.style.display = 'flex';
+  closeBurgerMenu.style.display = 'none';
+});
