347de014a99758e92332f08d094031e122170e86
Dusan95z <dusan204z@gmail.com>
2022-08-12 18:00:52 +0200

    contact form validation styling

---
+++ pages/contact/contact.js
@@ -0,0 +1,53 @@
+// CONTACT FORM VALIDATION STYLING
+
+const contactForm = document.querySelector('.contact-form');
+const yourNameInput = document.getElementById('yourName');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
+const selectInput = document.getElementById('subject');
+const formBtn = document.querySelector('.contact-form-btn');
+
+formBtn.addEventListener('click', (e) => {
+  console.log(contactForm.checkValidity());
+  if (!yourNameInput.checkValidity()) {
+    yourNameInput.style.border = '3px solid red';
+  } else {
+    yourNameInput.style.border = '3px solid green';
+  }
+  if (!emailInput.checkValidity()) {
+    emailInput.style.border = '3px solid red';
+  } else {
+    emailInput.style.border = '3px solid green';
+  }
+  if (!messageInput.checkValidity()) {
+    messageInput.style.border = '3px solid red';
+  } else {
+    messageInput.style.border = '3px solid green';
+  }
+  if (!subject.checkValidity()) {
+    subject.style.border = '3px solid red';
+  } else {
+    subject.style.border = '3px solid green';
+  }
+  if (!contactForm.checkValidity()) {
+    formBtn.style.backgroundColor = 'red';
+  }
+  if (contactForm.checkValidity()) {
+    formBtn.textContent = 'Please wait...';
+    setTimeout(() => {
+      formBtn.textContent = 'Send message';
+    }, 5000);
+  }
+});
+
+formBtn.addEventListener('mouseover', (e) => {
+  if (!contactForm.checkValidity()) {
+    formBtn.style.backgroundColor = 'red';
+  } else {
+    formBtn.style.backgroundColor = '#55a155';
+  }
+});
+
+formBtn.addEventListener('mouseout', (e) => {
+  formBtn.style.backgroundColor = '#8b2a2a';
+});
