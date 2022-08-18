e496d47bbcf8a6a50115d3429109f38128de37dc
Dusan95z <dusan204z@gmail.com>
2022-08-18 02:19:55 +0200

    small code fix

--- pages/footballApp/footballApp.js
+++ pages/footballApp/footballApp.js
@@ -2,27 +2,25 @@
 
 const footballApp = document.querySelector('.footballApp');
 
-const getFootballData = () => {
+document.addEventListener('DOMContentLoaded', (e) => {
   fetch('../../../data.json')
     .then((res) => res.json())
     .then((data) => {
-      console.log(data)
-      data.forEach(item => {
+      console.log(data);
+      data.forEach((item) => {
         footballApp.innerHTML += `
-          <div class="footballApp-clubCard">
-          <div class="footballApp-clubCard-image">
-            <img src="${item.image}" alt="logo" />
-          </div>
-          <h2 class="footballApp-clubCard-name">${item.name}</h2>
-          <p class="footballApp-clubCard-p">Stadium - ${item.stadium}</p>
-          <p class="footballApp-clubCard-p">Titles - ${item.titles}</p>
-          <p class="footballApp-clubCard-p">Best player - ${item.best_player}</p>
-          <button type="button" class="footballApp-clubCard-btn">
-            See More
-          </button>
-        </div>`
-      })
+        <div class="footballApp-clubCard">
+        <div class="footballApp-clubCard-image">
+          <img src="${item.image}" alt="logo" />
+        </div>
+        <h2 class="footballApp-clubCard-name">${item.name}</h2>
+        <p class="footballApp-clubCard-p">Stadium - ${item.stadium}</p>
+        <p class="footballApp-clubCard-p">Titles - ${item.titles}</p>
+        <p class="footballApp-clubCard-p">Best player - ${item.best_player}</p>
+        <button type="button" class="footballApp-clubCard-btn">
+          See More
+        </button>
+      </div>`;
+      });
     });
-};
-
-getFootballData();
+});
