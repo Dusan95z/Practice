1214b2b8b2e020a3b880b42fac88591f09fbb52e
Dusan95z <dusan204z@gmail.com>
2022-08-25 01:18:37 +0200

    gym plan done test

--- pages/gymPlan/gymPlan.js
+++ pages/gymPlan/gymPlan.js
@@ -4,24 +4,31 @@ window.addEventListener('hashchange', (e) => {
   switch (window.location.hash) {
     case '#monday':
       showDay('monday');
+      showModals('monday');
       break;
     case '#tuesday':
       showDay('tuesday');
+      showModals('tuesday');
       break;
     case '#wednesday':
       showDay('wednesday');
+      showModals('wednesday');
       break;
     case '#thursday':
       showDay('thursday');
+      showModals('thursday');
       break;
     case '#friday':
       showDay('friday');
+      showModals('friday');
       break;
     case '#saturday':
       showDay('saturday');
+      showModals('saturday');
       break;
     case '#sunday':
       showDay('sunday');
+      showModals('sunday');
       break;
   }
 });
@@ -29,11 +36,41 @@ window.addEventListener('hashchange', (e) => {
 const showDay = (dayName) => {
   days.forEach((day) => {
     if (day.classList.contains(`gymPlan-${dayName}`)) {
-      day.classList.add('active-day');
+      day.classList.add('active-day', 'tracker');
     } else {
-      day.classList.remove('active-day');
+      day.classList.remove('active-day', 'tracker');
     }
   });
 };
 
 showDay('monday');
+
+// MODAL
+
+const gymPlan = document.querySelector('.gymPlan');
+const modalMain = document.querySelector('.modal');
+const modals = document.querySelectorAll('.modal-day');
+
+const showModals = (dayName) => {
+  modals.forEach((modal) => {
+    if (modal.classList.contains(`modal-${dayName}`)) {
+      modal.classList.add('tracker', 'showModalDay');
+    } else {
+      modal.classList.remove('tracker', 'showModalDay');
+    }
+  });
+};
+
+showModals('monday');
+
+gymPlan.addEventListener('click', (e) => {
+  if (e.target.classList.contains('gymPlan-day-btn')) {
+    modalMain.classList.add('modal-active');
+
+    modalMain.addEventListener('click', (e) => {
+      if (e.target.classList.contains('fa-x')) {
+        modalMain.classList.remove('modal-active');
+      }
+    });
+  }
+});
